8b1209c4f0b634ffcf9232d3c67c7901038ad2bc
Mahmudul Hasan <mh00471@my.westga.edu>
2024-03-07 18:36:08 -0500

    created FurnitureDepot DB Connection class

---
+++ DAL/FurnitureDepotDBConnection.cs
@@ -0,0 +1,26 @@
+﻿using System.Data.SqlClient;
+
+namespace FurnitureDepot.DAL
+{
+    /// <summary>
+    /// Get a connection object. 
+    /// </summary>
+    public class FurnitureDepotDBConnection
+    {
+        /// <summary>
+        /// Gets the connection.
+        /// </summary>
+        /// <returns></returns>
+        public static SqlConnection GetConnection()
+        {
+            string connectionString =
+                "Data Source=localhost;Initial Catalog=cs6232-g2;" +
+                // "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=cs6232-g2;" +
+                "Integrated Security=True";
+
+
+            SqlConnection connection = new SqlConnection(connectionString);
+            return connection;
+        }
+    }
+}
