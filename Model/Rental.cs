228128197045ae44f20e8a11adc9c90e8f4726fb
Mahmudul Hasan <mh00471@my.westga.edu>
2024-03-29 00:18:56 -0400

    added Rental and RentalItem model classes

---
+++ Model/Rental.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FurnitureDepot.Model
+{
+    /// <summary>
+    /// Rental Class
+    /// </summary>
+    internal class Rental
+    {
+        public int RentalId { get; set; }
+        public int CustomerId { get; set; }
+        public DateTime DateRented { get; set; }
+        public DateTime? DateReturned { get; set; }
+        public List<RentalItem> RentalItems { get; set; }
+    }
+}
---
+++ Model/RentalItem.cs
@@ -0,0 +1,15 @@
+﻿namespace FurnitureDepot.Model
+{
+    /// <summary>
+    /// RentalItem Class
+    /// </summary>
+    internal class RentalItem
+    {
+        public int RentalItemId { get; set; }
+        public int FurnitureId { get; set; }
+        public string FurnitureName { get; set; }
+        public string Description { get; set; }
+        public decimal DailyRentalRate { get; set; }
+        public int Quantity { get; set; }
+    }
+}
