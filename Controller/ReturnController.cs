480ac6e326c617b3a1f4532dcca937c99542186c
kr00277 <kr00277@my.westga.edu>
2024-04-19 15:52:53 -0500

    Creating return history user control

--- Controller/ReturnController.cs
+++ Controller/ReturnController.cs
@@ -4,18 +4,49 @@ using System.Collections.Generic;
 
 namespace FurnitureDepot.Controller
 {
+    /// <summary>
+    /// Return Controller
+    /// </summary>
     public class ReturnController
     {
         private readonly ReturnDAL _returnDAL;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ReturnController"/> class.
+        /// </summary>
         public ReturnController()
         {
             _returnDAL = new ReturnDAL();
         }
 
+        /// <summary>
+        /// Gets the rental items for return by transaction identifier.
+        /// </summary>
+        /// <param name="rentalTransactionID">The rental transaction identifier.</param>
+        /// <returns></returns>
         public List<RentalItem> GetRentalItemsForReturnByTransactionId(int rentalTransactionID)
         {
             return _returnDAL.GetRentalItemsByTransactionId(rentalTransactionID);
+        }
+
+        /// <summary>
+        /// Gets the return transactions by member identifier.
+        /// </summary>
+        /// <param name="memberID">The member identifier.</param>
+        /// <returns></returns>
+        public List<ReturnTransaction> GetReturnTransactionsByMemberID(int memberID)
+        {
+            return _returnDAL.GetReturnTransactionsByMemberID(memberID);
+        }
+
+        /// <summary>
+        /// Gets the returned items by transaction identifier.
+        /// </summary>
+        /// <param name="returnTransactionID">The return transaction identifier.</param>
+        /// <returns></returns>
+        public List<ReturnedItem> GetReturnedItemsByTransactionId(int returnTransactionID)
+        {
+            return _returnDAL.GetReturnedItemsByTransactionId(returnTransactionID);
         }
     }
 }
--- DAL/ReturnDAL.cs
+++ DAL/ReturnDAL.cs
@@ -1,12 +1,20 @@
 ﻿using FurnitureDepot.Model;
+using System;
 using System.Collections.Generic;
-using System.Data;
 using System.Data.SqlClient;
 
 namespace FurnitureDepot.DAL
 {
+    /// <summary>
+    /// ReturnDAL Class
+    /// </summary>
     public class ReturnDAL
     {
+        /// <summary>
+        /// Gets the rental items by transaction identifier.
+        /// </summary>
+        /// <param name="rentalTransactionID">The rental transaction identifier.</param>
+        /// <returns></returns>
         public List<RentalItem> GetRentalItemsByTransactionId(int rentalTransactionID)
         {
             List<RentalItem> rentalItems = new List<RentalItem>();
@@ -48,5 +56,86 @@ namespace FurnitureDepot.DAL
             return rentalItems;
         }
 
+        /// <summary>
+        /// Gets the returned items by transaction identifier.
+        /// </summary>
+        /// <param name="returnTransactionID">The return transaction identifier.</param>
+        /// <returns></returns>
+        public List<ReturnedItem> GetReturnedItemsByTransactionId(int returnTransactionID)
+        {
+            List<ReturnedItem> returnedItems = new List<ReturnedItem>();
+
+            using (SqlConnection connection = FurnitureDepotDBConnection.GetConnection())
+            {
+                string query = @"
+                    SELECT ReturnedItemID, ReturnTransactionID, RentalItemID, QuantityReturned
+                    FROM ReturnedItem
+                    WHERE ReturnTransactionID = @ReturnTransactionID";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@ReturnTransactionID", returnTransactionID);
+
+                    connection.Open();
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            ReturnedItem returnedItem = new ReturnedItem
+                            {
+                                ReturnedItemID = reader.GetInt32(reader.GetOrdinal("ReturnedItemID")),
+                                ReturnTransactionID = reader.IsDBNull(reader.GetOrdinal("ReturnTransactionID")) ? null : (int?)reader.GetInt32(reader.GetOrdinal("ReturnTransactionID")),
+                                RentalItemID = reader.IsDBNull(reader.GetOrdinal("RentalItemID")) ? null : (int?)reader.GetInt32(reader.GetOrdinal("RentalItemID")),
+                                QuantityReturned = reader.IsDBNull(reader.GetOrdinal("QuantityReturned")) ? null : (int?)reader.GetInt32(reader.GetOrdinal("QuantityReturned"))
+                            };
+
+
+                            returnedItems.Add(returnedItem);
+                        }
+                    }
+                }
+            }
+
+            return returnedItems;
+        }
+
+        /// <summary>
+        /// Gets the return transactions by member identifier.
+        /// </summary>
+        /// <param name="memberID">The member identifier.</param>
+        /// <returns></returns>
+        public List<ReturnTransaction> GetReturnTransactionsByMemberID(int memberID)
+        {
+            List<ReturnTransaction> returnTransactions = new List<ReturnTransaction>();
+
+            using (SqlConnection connection = FurnitureDepotDBConnection.GetConnection())
+            {
+                string query = @"SELECT * FROM ReturnTransaction WHERE MemberID = @MemberID";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@MemberID", memberID);
+
+                    connection.Open();
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            ReturnTransaction returnTransaction = new ReturnTransaction
+                            {
+                                ReturnTransactionID = reader.GetInt32(reader.GetOrdinal("ReturnTransactionID")),
+                                EmployeeID = reader.GetInt32(reader.GetOrdinal("EmployeeID")),
+                                MemberID = reader.GetInt32(reader.GetOrdinal("MemberID")),
+                                ReturnDate = reader.IsDBNull(reader.GetOrdinal("ReturnDate")) ? (DateTime?)null : reader.GetDateTime(reader.GetOrdinal("ReturnDate")),
+                            };
+
+                            returnTransactions.Add(returnTransaction);
+                        }
+                    }
+                }
+            }
+
+            return returnTransactions;
+        }
     }
 }
